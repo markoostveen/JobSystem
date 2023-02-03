de680822619d32c02cc3dd708a6aadf365ffe51d
Mark Oostveen <mark.oostveen@incontrolsim.com>
2023-02-03 22:24:57 +0100

    Removed inline from functions inside atomic mutex

--- JobSystem/Src/JobSystem/AtomicMutex.h
+++ JobSystem/Src/JobSystem/AtomicMutex.h
@@ -4,27 +4,36 @@
 
 namespace JbSystem {
 	class mutex {
-		std::atomic<bool> flag{ false };
-
 	public:
 
-		inline bool try_lock() {
-			if (flag.exchange(true, std::memory_order_relaxed))
+		mutex() : _flag(false) {
+
+		}
+
+		~mutex() {
+			unlock();
+		}
+
+		bool try_lock() {
+			if (_flag.exchange(true, std::memory_order_relaxed))
 				return false;
 			std::atomic_thread_fence(std::memory_order_acquire);
 			return true;
 		}
 
-		inline void lock()
+		void lock()
 		{
-			while (flag.exchange(true, std::memory_order_relaxed));
+			while (_flag.exchange(true, std::memory_order_relaxed));
 			std::atomic_thread_fence(std::memory_order_acquire);
 		}
 
-		inline void unlock()
+		void unlock()
 		{
 			std::atomic_thread_fence(std::memory_order_release);
-			flag.store(false, std::memory_order_relaxed);
+			_flag.store(false, std::memory_order_relaxed);
 		}
+
+	private:
+		std::atomic<bool> _flag;
 	};
 }
