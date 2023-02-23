96372476d7b522d03d8fb41a7b534edf51c17133
Mark <mark.oostveen@incontrolsim.com>
2023-02-23 22:40:56 +0100

    Optimized timings

--- JobSystem/Src/JobSystem/WorkerThread.cpp
+++ JobSystem/Src/JobSystem/WorkerThread.cpp
@@ -103,7 +103,7 @@ void JbSystem::JobSystemWorker::KeepAliveLoop()
 
 		Active.store(false);
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(250));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 	_isRunning.store(false);
 }
